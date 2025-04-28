7efc69fa1321311a55db03e6769585019d1d762c
Dev-Everly <everlysantos98@gmail.com>
2025-04-28 19:46:29 -0300

    refatoração e teste de busca

---
+++ cypress/e2e/busca.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress"/>
+
+describe ('US-00 : Funcionalidade: Busca de Filme', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    });
+
+    it('Busca de Filme com sucesso', () => {
+        cy.get('#search-input').type('Batman')
+        cy.get('#search-button').click()
+        cy.get('#results-section').should('contain', 'Batman')
+    });
+
+    it('Deve buscar filmes com sucesso de uma lista ',() => {
+        cy.fixture('filmes').then((filmes) => {
+            cy.get('#search-input').type(filmes[0].titulo)
+            cy.get('#search-button').click()
+            cy.get('#results-section').should('contain', filmes[0].titulo)
+        })
+    })
+
+    it.only('Busca filmes com sucesso da lista inteira', () => {
+        cy.fixture('filmes').each((filmes) => {
+            cy.get('#search-input').clear().type(filmes.titulo)
+            cy.get('#search-button').click({force:true})
+            cy.get('#results-section').should('contain', filmes.titulo)
+        })
+    })
+
+
+})
